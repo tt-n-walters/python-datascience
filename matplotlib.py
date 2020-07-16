d9ade2cd032f70c82919f29e1eaea9c9b553c818
ttnw <nico.walters@techtalents.es>
2020-07-16 11:31:42 +0200

    Scatter plot

--- matplotlib.py
+++ matplotlib.py
@@ -5,17 +5,18 @@ sys.path.append(sys.path.pop(0))
 from matplotlib import pyplot
 import numpy as np
 import math
+import random
 
 x_coords = np.arange(-10, 11, 0.2)
-y_coords = list(map(math.sin, x_coords))
+y_coords = [random.random() * 10 for x in x_coords]
 
-# pyplot.style.use("ggplot")
-pyplot.style.use("dark_background")
+sizes = [random.choice([10, 20, 50, 100, 200]) for x in x_coords]
+all_colours = ["red", "orange", "blue", "green", "cyan", "purple"]
+colours = [random.choice(all_colours) for x in x_coords]
 
-pyplot.plot(x_coords, y_coords, "m-.")
+pyplot.style.use("ggplot")
 
-pyplot.xlim(-10, 10)
-pyplot.ylim(-3, 3)
+pyplot.scatter(x_coords, y_coords, s=sizes, c=colours, alpha=0.5)
 
 pyplot.show()
 
