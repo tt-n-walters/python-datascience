a267c551a12dfd53d50a29d02e34414c1ca711a1
ttnw <nico.walters@techtalents.es>
2020-07-21 10:36:10 +0200

    Chess data processing v0.2

--- chess_data.py
+++ chess_data.py
@@ -25,27 +25,40 @@ games_after_2018 = games[games.created_at < date_point]
 #   BONUS: Might look into the set() data structure
 player_names = []
 for name in games.white_id:
-    # is_already = False
-    # for n in player_names:
-    #     if name == n:
-    #         is_already = True
-    #         break
-    # if not is_already:
-        player_names.append(name)
+    player_names.append(name)
 
 for name in games.black_id:
-    # if not name in player_names:
-        player_names.append(name)
+    player_names.append(name)
 
-print(len(player_names))
 player_names = list(set(player_names))
-print(len(player_names))
 
+print("Finished list of unique names.")
 
 # Count wins for each player
 #   Iterate over all unique player names, for each name count the number of wins
 #   in the dataframe. eg. len(games[games.white_id == name][games.winner == "white"])
+player_games = {}
+for i, name in enumerate(player_names):
+    if i % 10 == 0:
+        print("Processed", i, "players.", end="\r")
+
+    # Extract all the games played in
+    games_as_white = games[games.white_id == name]
+    games_as_black = games[games.black_id == name]
+    won_white = games_as_white[games.winner == "white"]
+    won_black = games_as_black[games.winner == "black"]
+
+    count_won = len(won_white) + len(won_black)
+    player_games[name] = count_won
+
+print("Finished counting player wins.")
 
 # Find the most successful player
 #   If a dictionary is chosen to represent the number of wins for each player, then
 #   max() can be used with a key-function to find the most successful player.
+
+def get_player_wins(name):
+    return player_games[name]
+
+winner = max(player_games, key=get_player_wins)
+print(winner, get_player_wins(winner))
